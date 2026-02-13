5845747c440d6cc43af2d82ff63c163389f2d7cc
valla2345 <yui2345@naver.com>
2026-02-13 11:22:39 +0900

    도메인 경로 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,29 +4,28 @@ import { useState } from "react"
 
 function App() {
 
-const [data, setData] = useState()
-const [obj, setObj] = useState()
-const sendToServer = () => {
-    console.log('send to server', data)
-    // front end에서 back end로 데이터 전송
-    axios.post('http://localhost:3000/getData', {data : data})
-    .then((res) => {
+  const [data, setData] = useState()
+  const [obj, setObj] = useState()
+  const sendToServer = () => {
+    axios.post('/getData', {
+      data: data
+    })
+      .then((res) => {
         console.log(res.data)
         setObj(res.data)
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err)
-    })
-    
-}
- 
+      })
+  }
+
   return (
     <>
-    <h1>
+      <h1>
         <p>Client에서 Server로 데이터 전송하기</p>
         <input type="text" onChange={(e) => setData(e.target.value)}></input>
         <button onClick={sendToServer}>전송</button>
-        <hr/>
+        <hr />
         <p>Server에서 Client로 넘어온 데이터</p>
         {obj?.status === 200 && <p>{obj.nick}님 환영합니다.</p>}
       </h1>
